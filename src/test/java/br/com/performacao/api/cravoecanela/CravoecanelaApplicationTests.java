d3690bc367d5cc41e0f3360675452403b901bb46
Viviane Lara <viviflara@gmail.com>
2022-09-13 20:16:48 -0300

    Initial commit

---
+++ src/test/java/br/com/performacao/api/cravoecanela/CravoecanelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.performacao.api.cravoecanela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CravoecanelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
