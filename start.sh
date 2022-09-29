90b054949a989c0dbb07ba12a54198de565574cd
PTuppy <110697838+PTuppy@users.noreply.github.com>
2022-09-29 04:36:31 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-nohup java -jar home/${NAME}-${VERSION}.jar > /home/log.txt 2>&1 &
+
+set -x
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
+
+set -x
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+set +x
+
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
+
+set -x
+
+nohup java -jar home//RELEASE/$BUILD_NUMBER/${NAME}-${VERSION}.jar > /home/log.txt 2>&1 &
 echo $! > /home/pid.file
